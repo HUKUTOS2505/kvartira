51ab89dca829cf27b4d1eacb15451dc2178ce91c
HUKUTOS2505 <zalesskiy-nikita@mail.ru>
2020-06-09 16:56:57 +0400

    валейкум

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-let button = document.querySelector('#button');
-let modal=document.querySelector('#modal');
-let close=document.querySelector('#close');
-button.addEventListener('click',function (){
-modal.classList.add('modal_active');
+$(document).ready(function () {
 
+$('#button').click(function () {
+    $('#modal').addClass('modal_active');
 });
-close.addEventListener('click',function () {
-modal.classList.remove('modal_active');
+    $('#close').click(function () {
+        $('#modal').removeClass('modal_active');
+    });
+
+
+
+       $('#up').click(function () {
+           $('html,body').animate({scrollTop:0},600)
+       });
+    $('#down').click(function () {
+        $('html,body').animate({scrollTop:$(document).height()-$(window).height()},600)
+    });
+
+        $('.slider').slick({
+
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: $('.arrows__left'),
+            nextArrow:$('.arrows__right'),
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+
+        ]
+        });
 });
